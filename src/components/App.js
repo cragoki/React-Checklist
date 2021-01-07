f308c6353ccdb7e8b93fb4736a91f5ac2aa9550e
Craig Rodger <craigrodger1@hotmail.com>
2021-01-07 16:56:44 +0100

    Removing old App.js File
    Removing old App.js File and adding basic HTML Checklist to the new App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 function App(){
-    return (<h1>Hello World</h1>);
+    return (
+        <div className="container">
+            <div className="checkboxContainer">
+                <input type="checkbox" id="react" name="react" value="React" />
+                <label for="react"> React</label>
+            </div>
+            <div className="checkboxContainer">
+                <input type="checkbox" id="node" name="node" value="Node JS" />
+                <label for="node"> Node.JS</label>
+           </div>
+           <div className="checkboxContainer">
+                <input type="checkbox" id="typeScript" name="typeScript" value="TypeScript" />
+                <label for="typeScript"> TypeScript</label>
+            </div>
+            <div className="checkboxContainer">
+                <input type="checkbox" id="mongoDB" name="mongoDB" value="MongoDB" />
+                <label for="mongoDB"> MongoDB</label>
+            </div>
+        </div>
+    );
 }
 
 export default App;
