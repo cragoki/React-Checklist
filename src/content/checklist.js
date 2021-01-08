28a5b4622ba80b9d9e5794909b4664509a71ef49
Craig Rodger <craigrodger1@hotmail.com>
2021-01-08 12:17:54 +0100

    Adding JSON File for data
    Adding JSON File for data to eventually pull into my components

---
+++ src/content/checklist.js
@@ -0,0 +1,29 @@
+const checkListContent = [
+    {
+        name:"react",
+        id:"react",
+        value:"React"
+    },
+    {
+        name:"es",
+        id:"es",
+        value:"ES6"
+    },
+    {
+        name:"node",
+        id:"node",
+        value:"Node JS"
+    },
+    {
+        name:"ts",
+        id:"ts",
+        value:"TypeScript"
+    },
+    {
+        name:"mongo",
+        id:"mongo",
+        value:"Mongo DB"
+    },
+]
+
+export default checkListContent
